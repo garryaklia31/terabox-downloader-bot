48429a903972739317b39bd47991fbf0e98b2950
garryaklia31 <158801619+garryaklia31@users.noreply.github.com>
2024-02-21 16:59:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-API_ID =   # api id
-API_HASH = ""  # api hash
+API_ID =  "28386099" # api id
+API_HASH = "a0057fbf1ca49ce5e9d26fd4afd6e78b"  # api hash
 
-BOT_TOKEN = ""  # bot token
+BOT_TOKEN = "6819695620:AAHpNgX852w0MktEN8fFGWd4RzW3PtMoOTY"  # bot token
 
 
 ## REDIS
@@ -9,6 +9,6 @@ HOST = ""  # redis host uri
 PORT = 6379  # redis port
 PASSWORD = ""  # redis password
 
-PRIVATE_CHAT_ID = -1001234567890  # CHAT WHERE YOU WANT TO STORE VIDEOS
-COOKIE = ""  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (dont use this)
-ADMINS = [1317173146]
+PRIVATE_CHAT_ID = -1002074421298  # CHAT WHERE YOU WANT TO STORE VIDEOS
+COOKIE = "PANWEB=1; TSID=m0RHbkAjLAp2ugYpZoIp9wgg7FHQY095; __bid_n=18be85f750d4f42dba4207; _ga=GA1.1.1627430852.1700410655; fbm_756701921753953=base_domain=.terabox.app; ndus=Y-v8LeYteHuiTHDIo5RLqixbQFofA06gObCb9DUE; __stripe_mid=ef178580-35ba-47ab-a706-1a000765950394f456; browserid=M8oIfy3G85e0v99eknqJDPeek9TdfnX963_PtXYvuosk_bIUUWm-W7sFEng=; lang=en; _ga_06ZNKL8C2E=GS1.1.1707915442.11.0.1707915554.60.0.0; csrfToken=EESlvx4P7Edqh6tOG30jTr7g"  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (dont use this)
+ADMINS = [1718738592]
